68840c7d71ae2e611be566dffbc9f2059f4d3920
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-07-09 22:09:53 -0300

    form configuration and new routes

--- playlib.py
+++ playlib.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,18 +8,30 @@ class Game:
         self.category = category
         self.console = console
 
+game1 = Game('Super-Mario', 'Adventure', 'NES')
+game2 = Game('Pokemon Gold', 'RPG', 'GBA')
+game3 = Game('Mortal Combat', 'Action', 'SNES')
+game_list = [game1, game2, game3]
 
 @app.route('/home')
 def home():
-    game1 = Game('Super-Mario', 'Adventure', 'NES')
-    game2 = Game('Pokemon Gold', 'RPG', 'GBA')
-    game3 = Game('Mortal Combat', 'Action', 'SNES')
-    game_list = [game1, game2, game3]
     return render_template('list.html',title='Games',game_list=game_list)
 
 @app.route('/new')
 def new():
     return render_template('new.html', title='New Game')
 
+@app.route('/create')
+def create():
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
+
+    game = Game(name, category, console)
+    game_list.append(game)
+
+    return render_template('list.html', title='Games', game_list=game_list)
+
 
 app.run()
+
